3f9d2e8658fa233ced7fe6a83c4084893db7727e
unknown <iamhasan9501@gmail.com>
2022-05-28 02:14:49 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#06b6d4",
+          secondary: "#0e7490",
+          accent: "#164e63",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
