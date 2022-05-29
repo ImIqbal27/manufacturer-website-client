76715801cb1a0087a8af92cef9bc72c1ec7bbee4
unknown <iamhasan9501@gmail.com>
2022-05-30 04:04:47 +0600

    get order data

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -5,16 +5,16 @@ const Dashboard = () => {
     return (
         <div class="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center justify-center">
+            <div class="drawer-content ">
                 {/* <!-- Page content here --> */}
-                <h2 className='text-3xl text-primary-focus'>Dashboard</h2>
+                <h2 className='text-3xl text-primary-focus '>My Dashboard</h2>
                 <Outlet></Outlet>
-                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open Dashboard</label>
 
             </div>
             <div class="drawer-side">
                 <label for="my-drawer-2" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                <ul class="menu p-4 overflow-y-auto w-60 bg-base-100 text-base-content">
 
                     <li><Link to='/dashboard/'>My Orders</Link></li>
                     <li><Link to='/dashboard/addreview'>My Reviews</Link></li>
--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/order?userEmail=${user.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data))
+        }
+
+    }, [user])
     return (
         <div>
-            <h2>My Orders</h2>
+            <h2>My Orders: {orders.length}</h2>
         </div>
     );
 };
