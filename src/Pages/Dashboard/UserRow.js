670c1815933e5f2568042ec886190ece50adb4fd
unknown <iamhasan9501@gmail.com>
2022-05-30 16:26:50 +0600

    admin

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -28,7 +28,7 @@ const UserRow = ({ user, refetch }) => {
             {/* <th>{ 1}</th> */}
             <td>{email}</td>
             <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
-            <td><button class="btn btn-xs">Remove User</button></td>
+            <td><button class="btn btn-xs">Delete User</button></td>
         </tr>
     );
 };
