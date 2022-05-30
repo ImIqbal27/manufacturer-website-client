6af2e27baf44649a9fdae77db414caedeb81cab4
unknown <iamhasan9501@gmail.com>
2022-05-30 15:33:57 +0600

    verifyJWT token

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,15 +1,32 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const MyOrders = () => {
     const [orders, setOrders] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/order?userEmail=${user.email}`)
-                .then(res => res.json())
+            fetch(`http://localhost:5000/order?userEmail=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                },
+
+            })
+            .then(res => {
+                console.log('ressss', res);
+                if (res.status === 401 || res.status === 403) {
+                    signOut(auth);
+                    localStorage.removeItem('accessToken');
+                    navigate('/');
+                }
+                return res.json()
+            })
                 .then(data => setOrders(data))
         }
 
